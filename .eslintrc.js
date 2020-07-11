30b92589086403395bcdfee18eb1781bd4f13bd2
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-07-11 16:35:29 -0700

    build(lint): disable 'react/jsx-props-no-spreading' rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: require.resolve('@hover/javascript/eslint'),
   rules: {
     'import/order': ['error', { 'newlines-between': 'always' }],
+    'react/jsx-props-no-spreading': 'off',
   },
 };
